49328f81edbf88497ce42928a0b64bc6cb7a6bda
tommy770221 <tommy770221@gmail.com>
2016-04-25 23:07:42 +0800

    1.完成 多個 指數資料 並寫到 result.csv 但是國家名稱有bug待修復

--- WebStockThree.js
+++ WebStockThree.js
@@ -1,39 +1,56 @@
 /**
  * 
  */
-
   var request = require("request");
   var fs = require("fs");
   var cheerio = require("cheerio");
   var iconv = require('iconv-lite');
   var BufferHelper = require('bufferhelper');
+  var md5 = require('md5');
+  var querystring = require("querystring");
+  
+  //指定主要指數
+  var country=['^FTSE','^NZ50','^SSEC'];
+  fs.writeFileSync("result.csv",'country,one week,one month,three month,six month \n');
+  
+  for(j=0;j<country.length;j++){
+  var date=new Date();
+  console.log(country[j]);  
+  console.log(querystring.stringify({symbolCode:country[j]}));
+  console.log((date.getFullYear()-1).toString());
+  console.log( date.getMonth() + 1);
+  console.log( date.getDate());
+ 
   request({
     url: "https://stock-ai.com/dailyDataQuery.php",
     method: "POST",
-    body: "a=c&symbolCode=%5ENZ50&from=2015%2F04%2F24&to=2016%2F04%2F24&hash=d41d8cd98f00b204e9800998ecf8427e",
+    body: "a=c&"+querystring.stringify({symbolCode:country[j]})+"&from="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&to="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&hash=d41d8cd98f00b204e9800998ecf8427e",
       headers: {
     	    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     	  },
       agentOptions: {
     	    ca: fs.readFileSync("./GeoTrustGlobalCA.crt")
     }
-  }, function(e,r,b) {
-    if(e || !b) 
-    {  console.log('error\n'+e);
-    console.log(r);
-    console.log(b);
-    	return; }
-   
-    var $ = cheerio.load(b);
-    var result = [];
-    var titles = $("#DivIXReFund_IX");
-    console.log(b);
-    console.log($(titles[0]).text());
-    var string=$(titles[0]).text();
+    }, function(e,r,b) {
+     if(e || !b) 
+     {  
+       console.log('error\n'+e);
+       console.log(r);
+       console.log(b);
+      return; 
+      }
+     var result =country[j]+',';
 
-   // for(var i=0;i<titles.length;i++) {
-   //   result.push($(titles[i]).text());
-   //   console.log($(titles[i]).text());
-   // }
-   // fs.writeFileSync("result.json", JSON.stringify(result));
-  });
+      console.log(b);
+      var dataJson=JSON.parse(b);
+      console.log(dataJson.v0);
+      var $ = cheerio.load(dataJson.v0+dataJson.v1+dataJson.v2+dataJson.v3);
+      var titles = $("span");
+  
+      for(var i=0;i<titles.length;i++) {
+        result=result+$(titles[i]).text()+',';
+        console.log($(titles[i]).text());
+      }
+      fs.appendFileSync("result.csv",result+'\n');
+   });
+  };
