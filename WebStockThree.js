d273be9f84b5022d009c3c3aaee30ed8cdea87d1
tommy770221 <tommy770221@gmail.com>
2016-04-26 22:11:50 +0800

    1.修改 指數名稱undefine問題

--- WebStockThree.js
+++ WebStockThree.js
@@ -10,21 +10,21 @@
   var querystring = require("querystring");
   
   //指定主要指數
-  var country=['^FTSE','^NZ50','^SSEC'];
+  var countrys=['^FTSE','^NZ50','^SSEC','^N225'];
   fs.writeFileSync("result.csv",'country,one week,one month,three month,six month \n');
   
-  for(j=0;j<country.length;j++){
+  for(country in countrys){
   var date=new Date();
-  console.log(country[j]);  
-  console.log(querystring.stringify({symbolCode:country[j]}));
+  console.log(country);  
+  console.log(querystring.stringify({symbolCode:country}));
   console.log((date.getFullYear()-1).toString());
   console.log( date.getMonth() + 1);
   console.log( date.getDate());
- 
+  (function(country) {
   request({
     url: "https://stock-ai.com/dailyDataQuery.php",
     method: "POST",
-    body: "a=c&"+querystring.stringify({symbolCode:country[j]})+"&from="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&to="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&hash=d41d8cd98f00b204e9800998ecf8427e",
+    body: "a=c&"+querystring.stringify({symbolCode:country})+"&from="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&to="+(date.getFullYear()-1).toString()+"%2F"+(date.getMonth()+ 1)+"%2F"+(date.getDate())+"&hash=d41d8cd98f00b204e9800998ecf8427e",
       headers: {
     	    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     	  },
@@ -39,9 +39,9 @@
        console.log(b);
       return; 
       }
-     var result =country[j]+',';
-
-      console.log(b);
+     var result =country+',';
+     console.log(country);
+   //   console.log(b);
       var dataJson=JSON.parse(b);
       console.log(dataJson.v0);
       var $ = cheerio.load(dataJson.v0+dataJson.v1+dataJson.v2+dataJson.v3);
@@ -52,5 +52,5 @@
         console.log($(titles[i]).text());
       }
       fs.appendFileSync("result.csv",result+'\n');
-   });
+   })})(countrys[country]);
   };
